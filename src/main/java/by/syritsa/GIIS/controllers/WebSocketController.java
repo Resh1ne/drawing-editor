67049c29efda41cd2f78e9af963dd067bfb4cbe7
Daniil Pototskiy <daniilpototskiy@mail.ru>
2025-03-23 21:10:31 +0300

    Added lab2

--- src/main/java/by/syritsa/GIIS/controllers/WebSocketController.java
+++ src/main/java/by/syritsa/GIIS/controllers/WebSocketController.java
@@ -4,10 +4,13 @@ import by.syritsa.GIIS.algorithms.Pixel;
 import by.syritsa.GIIS.algorithms.lb1.BresenhamAlgorithm;
 import by.syritsa.GIIS.algorithms.lb1.DDAAlgorithm;
 import by.syritsa.GIIS.algorithms.lb1.WuAlgorithm;
+import by.syritsa.GIIS.algorithms.lb2.BresenhamCircle;
+import by.syritsa.GIIS.algorithms.lb2.BresenhamEllipse;
+import by.syritsa.GIIS.algorithms.lb2.BresenhamHyperbola;
+import by.syritsa.GIIS.algorithms.lb2.BresenhamParabola;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,6 +49,35 @@ public class WebSocketController {
             } else {
                 List<Pixel> newPixels = DDAAlgorithm.generateLine(pixels.get(0).x, pixels.get(0).y, pixels.get(1).x, pixels.get(1).y);
                 messagingTemplate.convertAndSend("/topic/line3", newPixels);
+            }
+        }
+    }
+
+    @MessageMapping("/lab2")
+    public void receiveCirclePoints(@RequestBody JsonNode jsonData) {
+        List<Pixel> pixels = new ArrayList<Pixel>();
+        int parabolaPar = jsonData.get("parabolaPar").asInt();
+        for (JsonNode point : jsonData.get("points")) {
+            int x = point.get("x").asInt();
+            int y = point.get("y").asInt();
+            Pixel pixel = new Pixel(x, y);
+            pixels.add(pixel);
+            System.out.println("Point: x=" + x + ", y=" + y);
+        }
+
+        if (jsonData.has("algorithm")) {
+            if (jsonData.get("algorithm").asText().equals("BresenhamCircle")) {
+                List<Pixel> newPixels = BresenhamCircle.generateCircle(pixels.get(0).x, pixels.get(0).y, pixels.get(1).x, pixels.get(1).y);
+                messagingTemplate.convertAndSend("/topic/line1", newPixels);
+            } else if (jsonData.get("algorithm").asText().equals("BresenhamEllipse")) {
+                List<Pixel> newPixels = BresenhamEllipse.generateEllipse(pixels.get(0).x, pixels.get(0).y, pixels.get(1).x, pixels.get(1).y);
+                messagingTemplate.convertAndSend("/topic/line1", newPixels);
+            } else if (jsonData.get("algorithm").asText().equals("BresenhamEllipse")) {
+                List<Pixel> newPixels = BresenhamHyperbola.generateHyperbola(pixels.get(0).x, pixels.get(0).y, pixels.get(1).x, pixels.get(1).y);
+                messagingTemplate.convertAndSend("/topic/line1", newPixels);
+            } else {
+                List<Pixel> newPixels = BresenhamParabola.drawParabola(pixels.get(0).x, pixels.get(0).y, parabolaPar);
+                messagingTemplate.convertAndSend("/topic/line1", newPixels);
             }
         }
     }
