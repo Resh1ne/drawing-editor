c2ce697efe5ddda1aace3190bd3ceb06a6cbbdb3
Daniil Pototskiy <daniilpototskiy@mail.ru>
2025-03-23 18:09:01 +0300

    Completed task 2

---
+++ src/main/java/by/syritsa/GIIS/algorithms/lb2/BresenhamCircle.java
@@ -0,0 +1,42 @@
+package by.syritsa.GIIS.algorithms.lb2;
+
+import by.syritsa.GIIS.algorithms.Pixel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BresenhamCircle {
+    public static List<Pixel> generateCircle(int xc, int yc, int x2, int y2) {
+        int radius = (int) Math.sqrt(Math.pow(x2 - xc, 2) + Math.pow(y2 - yc, 2));
+        List<Pixel> pixels = new ArrayList<>();
+
+        int x = 0, y = radius;
+        int d = 3 - 2 * radius;
+
+        while (x <= y) {
+            addSymmetricPixels(pixels, xc, yc, x, y);
+            if (d < 0) {
+                d += 4 * x + 6;
+            } else {
+                d += 4 * (x - y) + 10;
+                y--;
+            }
+            x++;
+        }
+        return pixels;
+    }
+
+    private static void addSymmetricPixels(List<Pixel> pixels, int xc, int yc, int x, int y) {
+        pixels.add(new Pixel(xc + x, yc + y, 1.0f));
+        pixels.add(new Pixel(xc - x, yc + y, 1.0f));
+        pixels.add(new Pixel(xc + x, yc - y, 1.0f));
+        pixels.add(new Pixel(xc - x, yc - y, 1.0f));
+        pixels.add(new Pixel(xc + y, yc + x, 1.0f));
+        pixels.add(new Pixel(xc - y, yc + x, 1.0f));
+        pixels.add(new Pixel(xc + y, yc - x, 1.0f));
+        pixels.add(new Pixel(xc - y, yc - x, 1.0f));
+    }
+}
+
+
+
---
+++ src/main/java/by/syritsa/GIIS/algorithms/lb2/BresenhamHyperbola.java
@@ -0,0 +1,57 @@
+package by.syritsa.GIIS.algorithms.lb2;
+
+import by.syritsa.GIIS.algorithms.Pixel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BresenhamHyperbola {
+    public static List<Pixel> generateHyperbola(int xc, int yc, int x2, int y2) {
+        List<Pixel> pixels = new ArrayList<>();
+
+        int a = Math.abs(x2 - xc);
+        int b = Math.abs(y2 - yc);
+
+        int x = a, y = 0;
+        int a2 = a * a, b2 = b * b;
+        int fx = 2 * b2 * x, fy = 2 * a2 * y;
+        float d = b2 - a2 * (1 + 2 * b);
+
+        while (fx > fy) {
+            addSymmetricPixels(pixels, xc, yc, x, y);
+            y++;
+            fy += 2 * a2;
+            if (d < 0) {
+                d += b2 + fy;
+            } else {
+                x++;
+                fx -= 2 * b2;
+                d += b2 + fy - fx;
+            }
+        }
+
+        d = b2 * (x + 0.5f) * (x + 0.5f) + a2 * (y - 1) * (y - 1) - a2 * b2;
+        while (x < 2 * a) {
+            addSymmetricPixels(pixels, xc, yc, x, y);
+            x++;
+            fx -= 2 * b2;
+            if (d > 0) {
+                d += a2 - fx;
+            } else {
+                y++;
+                fy += 2 * a2;
+                d += a2 - fx + fy;
+            }
+        }
+
+        return pixels;
+    }
+
+    private static void addSymmetricPixels(List<Pixel> pixels, int xc, int yc, int x, int y) {
+        pixels.add(new Pixel(xc + x, yc + y, 1.0f));
+        pixels.add(new Pixel(xc + x, yc - y, 1.0f));
+        pixels.add(new Pixel(xc - x, yc + y, 1.0f));
+        pixels.add(new Pixel(xc - x, yc - y, 1.0f));
+    }
+}
+
